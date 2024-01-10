10e803cc69bda82349354265ec5170ad01b5b233
JonatanDrysen <jojodrysen@gmail.com>
2024-01-10 10:33:52 +0100

    add task 2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@
 // task 1:
 const myAge = 21; console.log(`My age is ${myAge} years old!`);
 
+// task 2: 
+
+// 'var' is optionally initialized and globally scoped
+var manufacturer = "Mazda"
+// 'let' is optionally initialized and block-scoped
+let model = "Miata"
+// 'const' needs to be initialized and cannot be reassigned using '=' and is also block-scoped
+const modelYear = 1996
+console.log(`my car is a ${manufacturer} ${model} from ${modelYear}.`)
